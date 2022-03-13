276901267018d4b6fe476f7b8a17dc66818f27c2
suhas1git <raj.suhaskb@gmail.com>
2022-03-13 22:24:02 +0530

    a

--- src/main/java/com/wcs/demo/DemoApplication.java
+++ src/main/java/com/wcs/demo/DemoApplication.java
@@ -2,9 +2,18 @@ package com.wcs.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome to WCS Spring APP";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
